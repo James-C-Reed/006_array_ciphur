69ac6944ea4e6127d7415c6d801664bc5c3fc203
compsci <compsci@compsci>
2017-10-20 10:45:53 -0600

    working exept for z wrap

---
+++ src/caesar/BeginHere.java
@@ -0,0 +1,23 @@
+package caesar;
+
+import java.util.Scanner;
+
+public class BeginHere {
+	
+	public static void main(String[] args) {
+		Scanner kbd = new Scanner(System.in);
+		
+		CaesarCiphur myCiphur = new CaesarCiphur();
+		
+		System.out.println("Please enter plain text to ciphur.");
+		String plainText = kbd.nextLine();
+		
+		System.out.println("Please enter a key:");
+		int key = kbd.nextInt();
+		System.out.println("Your key is: " + key);
+		
+		String codedText = myCiphur.encode(plainText);
+		System.out.println("Your secret message is:" + codedText);
+		
+	}
+}
---
+++ src/caesar/CaesarCiphur.java
@@ -0,0 +1,41 @@
+package caesar;
+
+public class CaesarCiphur {
+	
+	private String alpha = "abcdefghijklmnopqrstuvwxyz";
+
+	public String encode(String plainText, int key) {
+		
+		String secretText = "";
+		int index, keyadded;
+		
+		System.out.println("Test alpha length = " + alpha.length());
+		System.out.println("The first char is: " + alpha.charAt(0));
+		System.out.println("The first char is: " + alpha.charAt(alpha.length()-1));
+		
+		for (int i = 0; i < alpha.length(); i++) {	
+			System.out.println(alpha.charAt(i));	
+		}
+		System.out.println("\n\n");
+		
+		for (int i = 0; i < plainText.length(); i++) {
+			
+			System.out.println(plainText.charAt(i));
+			
+		}
+		
+		for (int i = 0; i < plainText.length(); i++) {
+			
+			index = alpha.indexOf(plainText.charAt(i));
+			System.out.println("index = " + index);
+			
+			keyAdded = index + key;
+			System.out.println("Encoded text is: " + alpha.charAt(keyAdded));
+			
+		}
+		
+		return secretText;
+
+	}
+	
+}
