8e436137f1cfa574e37e85ee707f080be02b0670
compsci <compsci@compsci>
2017-10-30 10:10:53 -0600

    Fixed

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -1,3 +1,5 @@
+
+
 package caesar;
 
 import java.util.Scanner;
@@ -5,60 +7,64 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class BeginHere {
-	
+
 	public static void main(String[] args) {
-		
-		/*String msg = "Would you like to encode a plain text message?";
-		int confirm = JOptionPane.showConfirmDialog(null, msg);
-		*/
-		
-		Object[] selectionArray = {"Encoder", "Decoder", "Cracker", "Exit"};
+		/* Doug's example of drop-down JOP, with adds by Mr. M */
+		//Object is top class in Java, used when you are not sure what type might be used --
+		//using a String, int, class object, or what? -- so use Object [] array to cover anything
+		Object[] selectionsArray = { "Encoder", "Decoder", "Cracker", "Exit", "late addition" };
 		String showFirst = "Encoder";
 		String selection = (String) JOptionPane.showInputDialog(
-				null,
-				"Message: Select a utility: ",
-				"Doug's Example",
-				JOptionPane.PLAIN_MESSAGE,
-				null,
-				selectionArray
+				null, 
+				"Message: Select a Utility: ",
+				"Title: Doug's Example", 
+				JOptionPane.PLAIN_MESSAGE, 
+				null, 
+				selectionsArray, 
 				showFirst);
-		
-		if (selection == null) selection = "Cancelled";
+		if (selection == null) selection = "Cancelled";// .equals doesn't work, null is not a String
 		JOptionPane.showMessageDialog(null, "You have selected: " + selection);
-		
-		switch (selection) {
+
+		switch (selection) { // switch on String allowed in Java 7 and higher
 		case "Encoder":
 			JOptionPane.showMessageDialog(null, 
 					"We are now in the Encoder switch block - add some code!");
 			break;
+			//break means all done, leave the switch block - almost always use break
+			//if no break, execution continues for all remaining cases
 		case "Decoder":
 			JOptionPane.showMessageDialog(null, 
-					"We are now in the Decoder switch block - add some code!");
+				"We are now in the Decoder switch block - add some code!");
 			break;
 		case "Cracker":
 			JOptionPane.showMessageDialog(null, 
-					"We are now in the Cracker switch block - add some code!");
+				"We are now in the Cracker switch block - add some code!");
 			break;
 		case "Exit":
 			JOptionPane.showMessageDialog(null, 
-					"We are now in the Exit switch block - add some code to get us out!");
+				"We are now in the Exit switch block - so add some code already to get us out of here!");
+			System.exit(0);
 			break;
+		default:
+			JOptionPane.showMessageDialog(null, 
+				"We are now in the Unknown Case or Cancelled switch block - what do you want to do?");
 		}
+		//after the 'break' continue with the following code
 		
+		//OK, back to our earlier code ...
 		Scanner kbd = new Scanner(System.in);
-		
-		CaesarCiphur myCiphur = new CaesarCiphur();
-		
-		String msg = "Please enter plain text to ciphur.";
-		//System.out.println("Please enter plain text to ciphur.");
+		CaesarCipher myCipher = new CaesarCipher();
+
+		System.out.println("Please enter the plain text to encode:");
 		String plainText = kbd.nextLine().toLowerCase();
-		
-		String msg = 
-		//System.out.println("Please enter a key:");
+
+		System.out.println("Please enter the key:");
 		int key = kbd.nextInt();
-		
-		String codedText = myCiphur.encode(plainText, key);
-		//System.out.println("Your secret message is: " + codedText);
-		
+		// System.out.println("Your key is: " + key);
+
+		String codedText = myCipher.encode(plainText, key);
+		System.out.println("Your secret message is: " + codedText);
+
 	}
+
 }
