2e0009418fdb75301142f186ff271c4339d7d785
compsci <compsci@compsci>
2017-10-30 10:00:20 -0600

    JOP drop down added

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -12,15 +12,39 @@ public class BeginHere {
 		int confirm = JOptionPane.showConfirmDialog(null, msg);
 		*/
 		
-		/*Object[] possibilities = {"encode", "decode", "crack", "exit"};
-		String s = (String)JOptionPane.showInputDialog(
+		Object[] selectionArray = {"Encoder", "Decoder", "Cracker", "Exit"};
+		String showFirst = "Encoder";
+		String selection = (String) JOptionPane.showInputDialog(
 				null,
-				"Please make your selection",
-				"Start Here",
+				"Message: Select a utility: ",
+				"Doug's Example",
 				JOptionPane.PLAIN_MESSAGE,
-				possibilities,
-				"encode");
-		*/
+				null,
+				selectionArray
+				showFirst);
+		
+		if (selection == null) selection = "Cancelled";
+		JOptionPane.showMessageDialog(null, "You have selected: " + selection);
+		
+		switch (selection) {
+		case "Encoder":
+			JOptionPane.showMessageDialog(null, 
+					"We are now in the Encoder switch block - add some code!");
+			break;
+		case "Decoder":
+			JOptionPane.showMessageDialog(null, 
+					"We are now in the Decoder switch block - add some code!");
+			break;
+		case "Cracker":
+			JOptionPane.showMessageDialog(null, 
+					"We are now in the Cracker switch block - add some code!");
+			break;
+		case "Exit":
+			JOptionPane.showMessageDialog(null, 
+					"We are now in the Exit switch block - add some code to get us out!");
+			break;
+		}
+		
 		Scanner kbd = new Scanner(System.in);
 		
 		CaesarCiphur myCiphur = new CaesarCiphur();
